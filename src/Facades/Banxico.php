617e3db5670f25b1281b5d141a9c17cf243dc601
Axel Pardemann <axel.pardemann@norse.blue>
2023-03-26 14:59:29 -0600

    fix the facade methods return signature

--- src/Facades/Banxico.php
+++ src/Facades/Banxico.php
@@ -3,14 +3,15 @@
 namespace NorseBlue\Banxico\Facades;
 
 use DateTimeInterface;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Facade;
 use NorseBlue\Banxico\BanxicoApiClient;
 use NorseBlue\Banxico\Series\BanxicoSeriesData;
 use NorseBlue\Banxico\Series\BanxicoSeriesMetadata;
 
 /**
- * @method static BanxicoSeriesData getSeriesData(string $id_serie, ?DateTimeInterface $startDate = null, ?DateTimeInterface $endDate = null)
- * @method static BanxicoSeriesMetadata getSeriesMetadata(string $id_serie)
+ * @method static Collection<int, BanxicoSeriesData> getSeriesData(string $id_serie, ?DateTimeInterface $startDate = null, ?DateTimeInterface $endDate = null)
+ * @method static Collection<int, BanxicoSeriesMetadata> getSeriesMetadata(string $id_serie)
  */
 final class Banxico extends Facade
 {
