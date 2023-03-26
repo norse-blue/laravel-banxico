cfefcee36fd71b1b1e977d8f07d4ce06ff0398db
Axel Pardemann <axel.pardemann@norse.blue>
2023-03-26 09:36:01 -0600

    setup pest

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,11 +2,12 @@
 
 declare(strict_types=1);
 
-namespace Tests;
+namespace NorseBlue\Banxico\Tests;
 
 use PHPUnit\Framework\TestCase as BaseTestCase;
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
 
-abstract class TestCase extends BaseTestCase
+abstract class TestCase extends OrchestraTestCase
 {
     //
 }
